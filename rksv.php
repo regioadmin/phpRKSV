02012ac43da69e9923bf28fa63fab78c9f42db45
Regioadmin <admin@regionalberatung.at>
2016-04-08 22:12:00 +0200

    Update and rename rskv.php to rksv.php

---
+++ rksv.php
@@ -0,0 +1,13 @@
+<?php
+ class rksv
+        {
+        var $testvariable = "12345";
+
+        function meineklasse()
+            {
+            # dieses "function" heisst wie die Klasse - sobald die
+            # Klasse mit NEW erstellt wird, wird diese function
+            # als erstes aufgerufen - ohne dass wir sie manuell
+            # starten
+            } 
+?>
