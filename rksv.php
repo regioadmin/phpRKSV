6c8cd797e6706a550b25ca9fe4ee8141df14007c
Regioadmin <admin@regionalberatung.at>
2016-04-12 16:15:58 +0200

    Update rksv.php

--- rksv.php
+++ rksv.php
@@ -1,19 +1,11 @@
 <?php
  class RSKV {
-  protected $value;
-    RK_SUITE(0),                                            //Registrierkassenalgorithmuskennzeichen
-    CASHBOX_ID(1),                                          //Kassen-ID
-    RECEIPT_IDENTIFIER(2),                                  //Belegnummer
-    RECEIPT_DATE_AND_TIME(3),                               //Beleg-Datum-Uhrzeit
-    SUM_TAX_SET_NORMAL(4),                                  //Betrag-Satz-Normal
-    SUM_TAX_SET_ERMAESSIGT1(5),                             //Betrag-Satz-Ermaessigt-1
-    SUM_TAX_SET_ERMAESSIGT2(6),                             //Betrag-Satz-Ermaessigt-2
-    SUM_TAX_SET_NULL(7),                                    //Betrag-Satz-Null
-    SUM_TAX_SET_BESONDERS(8),                               //Betrag-Satz-Besonders
-    ENCRYPTED_TURN_OVER_VALUE(9),                           //Stand-Umsatz-Zaehler-AES256-ICM
-    CERTIFICATE_SERIAL_NUMBER_OR_COMPANYID_AND_KEYID(10),   //Zertifikat-Seriennummer (or Ordnungszahl Unternehmen plus KEY-ID)
-    CHAINING_VALUE_PREVIOUS_RECEIPT(11),                    //Sig-Voriger-Beleg
-    SIGNATURE_VALUE(12);                                    //Signatur
+  
+  protected $cashBoxId;
+  protected $turnOverCounterAESKey;
+  protected $companyID;
+  
+    
 
   public function standardReceipt()
             {
